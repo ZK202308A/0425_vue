dff4191aca54fbffcf42d2a0f58946dbcbd0b022
zorock <zk202308A@gmail.com>
2024-04-25 15:29:45 +0900

    v1

--- src/components/counter/CountDisplay.vue
+++ src/components/counter/CountDisplay.vue
@@ -1,9 +1,11 @@
 <script setup>
 
+const props = defineProps(['num'])
+
 </script>
 
 <template>
-  <h1>COUNT DISPLAY </h1>
+  <h1>COUNT DISPLAY {{props.num}}</h1>
 </template>
 
 <style scoped>
--- src/components/counter/CountWrapper.vue
+++ src/components/counter/CountWrapper.vue
@@ -2,10 +2,13 @@
 
 import CountDisplay from "./CountDisplay.vue";
 import CountButtons from "./CountButtons.vue";
+import {ref} from "vue";
+
+const data = ref(3)
 </script>
 
 <template>
-  <CountDisplay/>
+  <CountDisplay :num=data />
   <CountButtons/>
 </template>
 
